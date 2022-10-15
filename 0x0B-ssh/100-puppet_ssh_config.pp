dcdfa3ede259a298a40414f11b96153e18aa97ab
“victornnamdii” <victorilodiuba@gmail.com>
2022-10-15 15:59:52 -0700

    Edit(3): 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,16 @@
 # ssh config with puppet
 
+include stdlib
+
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-  match => 'PasswordAuthentication yes',
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+  match  => 'PasswordAuthentication yes',
 }
 file_line { 'Declare identity file':
   ensure => present,
-  path  => '/etc/ssh/sshd_config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => 'IdentityFile',
+  path   => '/etc/ssh/sshd_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => 'IdentityFile',
 }
