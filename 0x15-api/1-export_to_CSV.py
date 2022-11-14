f3573166dfcec899039483992f27303f40df52e6
“victornnamdii” <victorilodiuba@gmail.com>
2022-11-14 14:32:11 +0100

    Add: 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Python script that returns information using a REST API
+"""
+
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    userId = argv[1]
+    user_info = requests.get('{}users/{}'.format(url, userId)).json()
+    employee_name = user_info.get('name')
+    all_tasks = requests.get('{}todos?userId={}'.format(url, userId)).json()
+
+    with open('{}.csv'.format(userId), 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for task in all_tasks:
+            csvwriter.writerow([int(userId), employee_name,
+                               task.get('completed'), task.get('title')])
