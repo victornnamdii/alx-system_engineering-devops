a5f655ec6a2ca418d613eb0e32851b404f6b322e
“victornnamdii” <victorilodiuba@gmail.com>
2022-11-29 21:50:20 +0100

    Edit: 0x16/100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Functions to adcquire info from API Reddit"""
-import requests
-after = None
+from requests import get
 
 
 def count_words(subreddit, word_list):
     """Count the titles found with wordlist in subreddit"""
     my_list = recurse(subreddit)
     my_dict = {}
+    count = 0
 
-    if my_list:
+    if my_list is not None:
         for word in word_list:
             my_dict[word] = 0
 
@@ -19,33 +19,36 @@ def count_words(subreddit, word_list):
             for iter in title_split:
                 for iter_split in word_list:
                     if iter.lower() == iter_split.lower():
+                        count = 1
                         my_dict[iter_split] += 1
-
+        if count == 0:
+            return
         for key, val in sorted(my_dict.items(),  key=lambda x: x[1],
                                reverse=True):
             if val != 0:
                 print("{}: {}".format(key, val))
-
-
-def recurse(subreddit, hot_list=[]):
-    """ recurse is a function that return hot list from
-        a subreddit"""
-    global after
-    headers = {'User-Agent': 'victornnamdii'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    response = requests.get(url, headers=headers, allow_redirects=False,
-                            params=parameters)
-    if response.status_code == 200:
-        prox = response.json().get('data').get('after')
-
-        if prox is not None:
-            after = prox
-            recurse(subreddit, hot_list)
-        list_titles = response.json().get('data').get('children')
-
-        for title_ in list_titles:
-            hot_list.append(title_.get('data').get('title'))
-        return hot_list
     else:
-        return (None)
+        return
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """
+        Returns a list containing the titles of all hot articles
+        for a given subreddit.
+        Returns None if subreddit doesn't exist.
+    """
+    parameters = {"after": after}
+    header = {"User-Agent": "victornnamdii"}
+    result = get('https://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                 headers=header, allow_redirects=False, params=parameters)
+    if result.status_code != 200:
+        return None
+
+    after = result.json().get('data').get('after')
+
+    for item in result.json().get('data').get('children'):
+        hot_list.append(item.get('data').get('title'))
+
+    if after is None:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
