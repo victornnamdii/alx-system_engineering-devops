534fd25df57e03e5fa2799e6919cc8473793487a
“victornnamdii” <victorilodiuba@gmail.com>
2022-11-29 19:37:25 +0100

    Add: 0x16/1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Contains the function number_of_subscribers
+"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+        Prints the titles of the first 10 hot posts listed for
+        a given subreddit.
+        Prints None if subreddit doesn't exist.
+    """
+    parameters = {"limit": 10}
+    header = {"User-Agent": "victornnamdii"}
+    result = get('https://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                 headers=header, allow_redirects=False, params=parameters)
+    if result.status_code == 404:
+        print(None)
+    else:
+        for item in result.json().get('data').get('children'):
+            print(item.get('data').get('title'))
